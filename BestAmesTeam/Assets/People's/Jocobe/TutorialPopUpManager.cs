16b880b707a4a6f03556293fabbad89603cf64fa
MUKILTEO\1720941 <1720941>
2026-03-20 10:11:27 -0700

    cool things im too tired to finish

--- BestAmesTeam/Assets/People's/Jocobe/TutorialPopUpManager.cs
+++ BestAmesTeam/Assets/People's/Jocobe/TutorialPopUpManager.cs
@@ -1,16 +1,43 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TutorialPopUpManager : MonoBehaviour
 {
-    public List<TutorialPopUp> tutorialPopUps = new List<TutorialPopUp>();
-}
+    //ItemBox tutorials;
+    //public float interactDistance = 3f;
 
-[System.Serializable]
-public class TutorialPopUp
-{
-    public string description;
-    public Sprite image;
-    public Sprite icon;
+
+    //void TryInteract(Ray ray)
+    //{
+    //    RaycastHit hit;
+
+    //    if (Physics.Raycast(ray, out hit, interactDistance))
+    //    {
+
+    //        Debug.Log("Ray hit: " + hit.collider.name);
+
+    //        if (hit.collider.GetComponent<ItemBox>() && tutorials == null)
+    //        {
+    //            PickupBox(hit.collider.GetComponent<ItemBox>());
+    //            return;
+    //        }
+    //    }
+    //}
+
+    //void Tutorial(ItemBox box)
+    //{
+    //    box.transform.SetParent(holdPoint);
+    //    box.transform.localPosition = Vector3.zero;
+    //    box.transform.localRotation = Quaternion.identity;
+
+    //    Rigidbody rb = box.GetComponent<Rigidbody>();
+    //    if (rb) rb.isKinematic = true;
+
+    //    box.GetComponent<Collider>().enabled = false;
+
+    //    Debug.Log("Picked up box");
+    //}
+
 }
