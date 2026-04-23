4d8e29767c2bf017993bcdac267a0aed8446c455
MUKILTEO\1722415 <1722415>
2026-04-23 10:14:15 -0700

    no message

--- BestAmesTeam/Assets/FollowBody.cs
+++ BestAmesTeam/Assets/FollowBody.cs
@@ -1,17 +1,24 @@
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 [ExecuteAlways]
 public class FollowBody : MonoBehaviour
 {
     public AccessoriesDictionary AccessoriesDictionary;
+    public RaceDictionary raceDictionary;
 
     public List<Transform> partsToFollow = new List<Transform>();
     public List<Transform> partsParent = new List<Transform>();
 
     public Transform mainBody;
+    public GameObject CharacterBase;
     public List<Transform> partsList = new List<Transform>();
 
+    private void Awake()
+    {
+        Setup();
+    }
 
     void LateUpdate()
     {
@@ -19,8 +26,7 @@ public class FollowBody : MonoBehaviour
 
         if (partsToFollow.Count == 0 || partsParent.Count == 0)
         {
-            Repair();
-            GetOutfits();
+            Setup();
         }
 
         int count = Mathf.Min(partsToFollow.Count, partsParent.Count);
@@ -34,13 +40,52 @@ public class FollowBody : MonoBehaviour
         }
     }
 
+    [ContextMenu("Setup")]
+    public void Setup()
+    {
+        GetOutfits();
+        Repair();
+    }
+
+    [ContextMenu("Clear")]
+    public void Clear()
+    {
+        while (this.transform.childCount > 2)
+        {
+            Transform child = this.transform.GetChild(2);
+            if (child.CompareTag("CharacterEdit"))
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+
+        partsList.Clear();
+    }
+
     [ContextMenu("GetOutfit")]
     public void GetOutfits()
     {
+        Clear();
+
+        // Spawn Shirt
+        GameObject shirt = Instantiate(AccessoriesDictionary.Shirts[Random.Range(0, AccessoriesDictionary.Shirts.Count)]).gameObject;
+        shirt.transform.parent = this.transform;
+        shirt.transform.localPosition = Vector3.zero;
+        shirt.transform.localScale = Vector3.one;
+
+        // Spawn Pants
+        GameObject pants = Instantiate(AccessoriesDictionary.Pants[Random.Range(0, AccessoriesDictionary.Pants.Count)]).gameObject;
+        pants.transform.parent = this.transform;
+        pants.transform.localPosition = Vector3.zero;
+        pants.transform.localScale = Vector3.one;
+
+
+        CharacterBase.GetComponent<SkinnedMeshRenderer>().material = raceDictionary.GetRandomRace();
+
         // Implementation for GetOutfit
-        for (int i = 0; i < partsList.Count; i++)
+        for (int i = 2; i < transform.childCount; i++)
         {
-            SkinnedMeshRenderer renderer = partsList[i].GetComponentInChildren<SkinnedMeshRenderer>();
+            SkinnedMeshRenderer renderer = transform.GetChild(i).GetComponentInChildren<SkinnedMeshRenderer>();
             if (renderer != null)
             {
                 renderer.material = AccessoriesDictionary.GetRandomColor();
@@ -53,6 +98,7 @@ public class FollowBody : MonoBehaviour
     {
         partsParent.Clear();
         partsToFollow.Clear();
+        partsList.Clear();
 
         // Build map from mainBody
         Dictionary<string, Transform> parentMap = new Dictionary<string, Transform>();
@@ -62,6 +108,15 @@ public class FollowBody : MonoBehaviour
             if (!parentMap.ContainsKey(t.name))
             {
                 parentMap.Add(t.name, t);
+            }
+        }
+
+        for (int i = 0;i < this.transform.childCount;i++)
+        {
+            Transform child = this.transform.GetChild(i);
+            if (child.CompareTag("CharacterEdit") && !partsList.Contains(child))
+            {
+                partsList.Add(child);
             }
         }
 
